7c049e317ff2d822ffc7ed32ef00a785ec2e7152
Sojith Sunny <sojithsunny29034@gmail.com>
2024-06-12 20:57:36 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,8 +6,7 @@ from io import BytesIO
 
 def run_main_app():  
     # st.set_page_config(page_title="Student Attendance Report", layout="wide")
-    import mew
-    mew.empty()
+    
     st.markdown("""
         <style>
             .reportview-container {margin-top: -2em;}
