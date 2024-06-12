0a5ff723e196839a28f50817404e8821d0b10017
Sojith Sunny <sojithsunny29034@gmail.com>
2024-06-12 20:54:31 +0530

    Update mew.py

--- mew.py
+++ mew.py
@@ -21,7 +21,7 @@ def login():
     if st.button("Login"):
         if authenticate(username, password):
             st.success("Login successful! Redirecting to main app...")
-            login().empty()
+            main.empty()
             # Redirect to main app
             index.run_main_app()
         else:
